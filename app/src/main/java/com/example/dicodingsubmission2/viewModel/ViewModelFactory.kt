5aa018fdd801687742348744c9ece7c80bdb81f6
narumiya1 <narumiiyamei@gmail.com>
2022-02-08 13:42:53 +0700

    final project

---
+++ app/src/main/java/com/example/dicodingsubmission2/viewModel/ViewModelFactory.kt
@@ -0,0 +1,16 @@
+package com.example.dicodingsubmission2.viewModel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.dicodingsubmission2.preferences.SettingPreferences
+
+class ViewModelFactory(private val pref: SettingPreferences) : ViewModelProvider.NewInstanceFactory() {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
+            return MainViewModel(pref) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class: " + modelClass.name)
+    }
+}
